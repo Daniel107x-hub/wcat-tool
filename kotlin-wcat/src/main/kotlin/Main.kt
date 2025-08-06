09d3639c26b2a9e8243507cee64c70d279d5a8a1
daniel107x-hub <DMejiaV310796@outlook.com>
2025-08-06 16:01:47 -0600

    Added -c, -l and -w options

--- kotlin-wcat/src/main/kotlin/Main.kt
+++ kotlin-wcat/src/main/kotlin/Main.kt
@@ -1,16 +1,47 @@
 package com.daniel107x
 
+import java.io.File
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main(args: Array<String>) {
+    if(args.isEmpty()) throw IllegalArgumentException("No args provided")
+    if(args.size < 2) throw IllegalArgumentException("Please provide an option and the file to process")
+    val option = args[0]
+    val fileName = args[1]
+    val file = validateFile(fileName)
+    val result: Int = when(option){
+        "-c" -> countBytesInFile(file)
+        "-l" -> countLinesInFile(file)
+        "-w" -> countWordsInFile(file)
+        else -> {
+            println("No option found")
+            return
+        }
     }
+    println("$result $fileName")
+}
+
+fun validateFile(fileName: String): File{
+    val file = File(fileName)
+    if(!file.exists()) throw IllegalArgumentException("The file doesn't exist")
+    return file
+}
+
+fun countBytesInFile(file: File): Int {
+    var bytes = 0
+    file.forEachBlock { buffer, size -> bytes += size}
+    return bytes
+}
+
+fun countLinesInFile(file: File): Int {
+    var lines = 0
+    file.forEachLine { line -> lines++ }
+    return lines
+}
+
+fun countWordsInFile(file: File): Int {
+    var words = 0
+    file.forEachLine { line -> if(!line.isEmpty()) words += line.trim().split(" ").size }
+    return words
 }
